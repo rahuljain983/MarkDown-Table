3688e90a957cafb6637a187125c2eb85814b1505
Rahul Jain <rahul.jain02@nagarro.com>
2017-03-06 21:42:11 +0530

    #2: Adding column functionality

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,15 +1,89 @@
 $(document).ready(function() {
-	var tableFormat  = "| Name   |      Age      |  Gender |\n" +
-						   "|----------|:-------------:|------:|\n" +
-						   "| Rahul |  26 | male |\n" +
-						   "| sachin |    28   |   male |\n" +
-						   "| Parashv | 1 |    tato |";
-						   						   					   
-	$('#addRow').click(function() {		
+
+	// To Generate the columns based on the input value eneterd
+	$("#btnGenerateColumn").on('click', function() {
+		var inputTable = $('#TableTxtArea').val()
+		if(ValidateInputData(inputTable))
+		{
+			GenerateColumns(inputTable);	
+		}
+	});				
+	
+    // To add a new Row			
+	$('#btnAddRow').click(function() {		
 		 AddRow();
 	});
+	
+	 //To add a new Column 
+	  $('#btnAddColumn').click(function() { 
+		 var columnValue =  $('#modalTxt').val();
+		 if(columnValue != "")
+		 {
+			$('#inputControls').append('<label>' + columnValue + ':</label> <input type ="text" placeholder="' + columnValue + '" id="' +columnValue +'txt"></input></br></br>');
+			$('#myModal').modal('toggle');
+			UpdatedTable =  UpdateMarkDownTable($('#TableTxtArea').val());
+			$('#TableTxtArea').val(UpdatedTable);		 
+		 }
+		 else 
+		 {
+			 alert('Please enter the column name.')
+		 }
+	 });
 });
 
+	//Method to validate whether the input table content is valid or not
+	function ValidateInputData(inputTable) 
+	{
+		if(inputTable === "") 
+		{
+			alert('No markdrop script for table is entered.');
+			return false;
+		}	
+		return true;
+	}
+	
+	//Method to generate the columns based on input table
+	function GenerateColumns(inputTable)
+	{
+		dataTable = ConvertTableStringToDataArray(inputTable);
+		var index = GetHeadingIndex(dataTable);	
+		$('#inputControls').empty();
+		if(index ==-1)
+		{
+			alert('No Columns Name have been entered. default columns are being generated');
+			for(var column = 0 ; column < dataTable[index+1].length; column++)
+			{
+				var columnValue = 'col' + column
+				$('#inputControls').append('<label>' + columnValue + ':</label> <input type ="text" placeholder="' + columnValue + '" id="' +columnValue +'txt"></input></br></br>');
+			}
+		}
+		else 
+		{
+			for(var column = 0 ; column < dataTable[index].length; column++)
+			{
+				var columnValue = dataTable[index][column].trim();
+				$('#inputControls').append('<label>' + columnValue + ':</label> <input type ="text" placeholder="' + columnValue + '" id="' +columnValue +'txt"></input></br></br>');
+			}
+		}
+	}
+	
+	//Method to get the Heading Index of the input table
+	function GetHeadingIndex(dataTable)
+	{
+	   for(var row = 0; row < dataTable.length; row++)
+	   {
+		   var count = 0;
+		   for(var column = 0; column < dataTable[row].length; column++)
+		   {
+			  if(dataTable[row][column].includes('-')) count++; 
+		   }
+		   if(count == dataTable[row].length) 
+		   {
+			   return row-1;
+		   }
+	   }
+	}
+	
 	function AddRow()
 	{
 		var inputTable = $('#TableTxtArea').val();
@@ -22,11 +96,12 @@ $(document).ready(function() {
 			dataTable = ConvertTableStringToDataArray(inputTable);
 		}
 		
-		var name = $('#NameTxt').val();
-		var gender = $('#GenderTxt').val();
-		var age = $('#AgeTxt').val();
-		
-		var dataRow = [name , gender , age];
+		var controls = $('#inputControls').children("input[type=text]");
+		var dataRow = [];
+		controls.each(function() 
+		{  
+			dataRow.push($(this).val()) 
+		});
 		dataTable.push(dataRow);
 		
 		var stringTable = ConvertTableDatatoTableString(dataTable);
@@ -34,18 +109,6 @@ $(document).ready(function() {
 		return dataTable;
 	}
 
-	function CheckMarkDownTable()
-	{
-		return true;
-	}
-	
-	//Method to validate whetehr the input data is valid or not.
-	function checkInputData() 
-	{
-		//
-		return true;
-	}
-	
 	//Method to convert the table data to string format to represent the data in markdown format.
 	function ConvertTableDatatoTableString(dataArray)
 	{
@@ -57,6 +120,8 @@ $(document).ready(function() {
 				result += "|" + dataArray[row][column];
 			}
 			result += "|";
+			
+			
 			if(row != dataArray.length-1)
 			{
 				result += "\n";
@@ -81,5 +146,35 @@ $(document).ready(function() {
 			}
 		}	
 		return tableData;
+	}
+	
+	//Update the markdown input table when new column is added
+	function UpdateMarkDownTable(inputTable)
+	{
+		var dataTable = ConvertTableStringToDataArray(inputTable);
+		var index = GetHeadingIndex(dataTable);	
+		//var tableRows = inputTable.length > 0 ? inputTable.split("\n") : [];
+		var UpdatedTable = [];
+		var newColumnIndex  = $('#inputControls').children("label").length-1
+		var columnName = $('#inputControls').children("label")[newColumnIndex].innerHTML
+		
+		for(var row = 0; row < dataTable.length; row++)
+		{
+			var dataRow = dataTable[row];
+			if(row == index)
+			{
+				dataRow.push(columnName.substring(0,columnName.length-1));
+			}
+			else if((index==-1 && row ==0) || (row == index + 1 && row!=0))
+			{
+				dataRow.push("-");
+			}
+			else 
+			{
+				dataRow.push("");
+			}
+			UpdatedTable.push(dataRow);
+		}	
+	return ConvertTableDatatoTableString(UpdatedTable);
 	}
 		
